40fd5c5e2f298e2430a569aeecb2ff4dfdd595ac
Andrei Voronin <impulsss@yandex.ru>
2012-03-20 18:26:58 +0400

    First Commit

---
+++ scripts/fc/data/fc.data.view.js
@@ -0,0 +1,19 @@
+(function ($) {
+	$.fc.define("fc.data.view", {
+		options: {
+
+		},
+
+		_create: function () {
+
+		},
+
+		page: function () {
+
+		},
+
+		refresh: function () {
+
+		}
+	});
+})(jQuery);
