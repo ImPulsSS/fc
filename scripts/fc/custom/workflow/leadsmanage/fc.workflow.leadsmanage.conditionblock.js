9883ab9009c8ae90bc3ed4a7adcf4704be6d1229
Andrei Voronin <impulsss@yandex.ru>
2012-04-06 10:42:34 +0400

    First commit

---
+++ scripts/fc/custom/workflow/leadsmanage/fc.workflow.leadsmanage.conditionblock.js
@@ -0,0 +1,64 @@
+(function ($) {
+	var __fc_workflow_nextBlock = {
+		__ClassName: "Fc.LeadsManage.Library.Objects.Workflow.CampaignActions.NextBlockItem"
+	};
+
+	$.fc.define("fc.workflow.leadsmanage.conditionblock", $.fc.workflow.iconblock, {
+		options: {
+			record: {}
+		},
+
+		_create: function () {
+			$.fc.workflow.block.prototype._create.call(this);
+
+			this.record = this.options.record;
+
+			this.details = new $.fc.workflow.leadsmanage.conditionfilter();
+			this.details.deserialize(this.options.record);
+		},
+
+		_serialize: function () {
+			this._trigger("beforeserialize");
+
+			var result = $.extend(true, {
+						__ClassName: "Fc.LeadsManage.Library.Objects.Workflow.ConditionBlockItem",
+						Uid: null,
+						ID: 0,
+						Name : "New condition block",
+						IsActive: true,
+						ConditionFilters: [],
+						TrueActions: [],
+						FalseActions: [],
+						HigherImportanceLevel: 0,
+						Debug : {
+							__ClassName: "Fc.LeadsManage.Library.Objects.Workflow.ConditionBlockDebugItem",
+							Uid: null,
+							ID: 0,
+							IsActive: true,
+							PreventDoAction: true,
+							MaxProcessedLeadCount: 100
+						}
+					}, this.record),
+				trueBranches = this.branches.left ?
+					this.branches.left.serialize() || [] :
+					[],
+				falseBranches = this.branches.right ?
+					this.branches.right.serialize() || [] :
+					[];
+
+			if (trueBranches.length) {
+				result.TrueActions.push($.extend(true, { Name: trueBranches[0].Name }, __fc_workflow_nextBlock));
+			}
+
+			if (falseBranches.length) {
+				result.FalseActions.push($.extend(true, { Name: falseBranches[0].Name }, __fc_workflow_nextBlock));
+			}
+
+			result = [ result ].concat(trueBranches, falseBranches);
+
+			this._trigger("serialize", result);
+
+			return result;
+		}
+	});
+})(jQuery);
---
+++ scripts/fc/custom/workflow/leadsmanage/fc.workflow.leadsmanage.conditionfilter.js
@@ -0,0 +1,85 @@
+(function ($) {
+	$.fc.widget("fc.workflow.leadsmanage.conditionfilter", $.fc.workflow, {
+		options: {
+		},
+
+		_create: function () {
+			$.fc.workflow.prototype._create.call(this);
+
+			this.record = this.options.record;
+		},
+
+		_init: function () {
+			this.ifblock = new $.fc.workflow.iconblock({
+				text: 'if block description',
+				className: "fc-workflow-root",
+				iconDirection: "left",
+				icon: "fc-workflow-icon-if",
+				workflow: this
+			});
+
+			this.thenblock = new $.fc.workflow.iconblock({
+				text: 'then block description',
+				className: "fc-workflow-root",
+				iconDirection: "left",
+				icon: "fc-workflow-icon-then",
+				workflow: this
+			});
+
+			this.elseblock = new $.fc.workflow.iconblock({
+				text: 'else block description',
+				className: "fc-workflow-root",
+				iconDirection: "left",
+				icon: "fc-workflow-icon-else",
+				workflow: this
+			});
+		},
+
+		_addBlock: function (record, parent, branch, nextBlock) {
+			var	self = this,
+				result = new $.fc.workflow.leadsmanage.block($.extend(true, {}, self.options.defaultBlockOptions, {
+						text: record.Name,
+						record: record,
+						workflow: self
+					})).addChild({
+						left: nextBlock || new $.fc.workflow.add({ workflow: self }),
+						right: new $.fc.workflow.add({ workflow: self })
+					});
+
+			parent.addChild(branch,
+				new $.fc.workflow.add({ workflow: self }).addChild({
+					left: result
+				})
+			);
+
+			return result;
+		},
+
+		addBlock: function (sender) {
+			var	self = this,
+				parent = sender.parent,
+				branch = sender.parentBranch;
+
+			$.fc.dialog.prompt("New condition", "Block name", "New condition", function (blockName) {
+				if (!blockName) {
+					return;
+				}
+
+				self._addBlock({ Name: blockName }, parent, branch, sender);
+
+				parent.render(branch);
+			});
+		},
+
+		_serialize: function () {
+			return this.record;
+		},
+
+		_deserialize: function (data) {
+			var self = this,
+				block = null,
+				children = {};
+
+		}
+	});
+})(jQuery);
